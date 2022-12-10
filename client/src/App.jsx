e18b17d1efc203bba8d1f5a8ee2ad054010d56d8
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-10 17:20:17 +0530

    Add routing to the react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './screens/Home';
 
 const App = () => {
   return (
-    <div>
-      <h1>Auctioneer App</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
