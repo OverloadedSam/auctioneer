84c5c8987745a1bd052273344a00ae97fab188b1
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-11 16:12:00 +0530

    Setup basic expresss server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const server = http.createServer(app);
+
+if (app.get('env') !== 'production') {
+  const morgan = require('morgan');
+  app.use(morgan('tiny'));
+}
+
+const port = process.env.PORT || 8000;
+server.listen(port, (error) => {
+  console.log(`The server is up and running at port: ${port}`);
+});
