4d60c4b578c119f3f6ed3aee7e08b06e8c223e45
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-12 20:51:42 +0530

    Define controller and its route to login user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const asyncHandler = require('../middlewares/asyncHandler');
-const { validateRegister } = require('../validations/user');
+const ErrorResponse = require('../utils/errorResponse');
+const { validateRegister, validateLogin } = require('../validations/user');
 
 // @route    POST /api/register
 // @desc     Register user to the DB
@@ -29,6 +30,47 @@ module.exports.registerUser = asyncHandler(async (req, res, next) => {
     .json({
       success: true,
       status: 201,
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        token,
+      },
+    });
+});
+
+// @route    POST /api/login
+// @desc     Login user by sending jwt
+// @access   Public
+module.exports.loginUser = asyncHandler(async (req, res, next) => {
+  const userPayload = { ...req.body };
+
+  const { error } = await validateLogin(userPayload);
+
+  if (error)
+    return res.status(400).json({
+      success: false,
+      status: 400,
+      message: error.message,
+      error,
+    });
+
+  const user = await User.findOne({ email: userPayload.email });
+  if (!user)
+    return next(new ErrorResponse(401, 'This email is not registered!'));
+
+  if (!(await user.matchPassword(userPayload.password)))
+    return next(new ErrorResponse(401, 'Invalid password!'));
+
+  const token = user.generateAuthToken();
+
+  res
+    .status(200)
+    .header('x-auth-token', token)
+    .json({
+      success: true,
+      status: 200,
       data: {
         id: user._id,
         name: user.name,
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { registerUser } = require('../controllers/users');
+const { registerUser, loginUser } = require('../controllers/users');
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
