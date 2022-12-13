03cc826d320547027f1fc863560b7ee6cb15930b
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-13 16:49:54 +0530

    Define auth slice to login user

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
+import auth from '../services/auth';
+
+const user = auth.getCurrentUser();
+const token = auth.getAuthToken();
+
+const preloadedState = {
+  auth: {
+    userLogin: {
+      isLoggedIn: !!(user && token),
+      user,
+      token,
+    },
+  },
+};
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  preloadedState,
 });
 
 export default store;
--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -54,8 +54,60 @@ const registerSlice = createSlice({
   },
 });
 
-export default combineReducers({
-  userRegister: registerSlice.reducer,
+const initialLoginState = {
+  loading: false,
+  success: false,
+  error: null,
+  user: null,
+};
+
+const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (payload, { fulfillWithValue, rejectWithValue }) => {
+    try {
+      const { data } = await http.post('/login', payload);
+      return fulfillWithValue(data);
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+
+      return rejectWithValue(message);
+    }
+  }
+);
+
+const loginSlice = createSlice({
+  name: 'auth/loginUser',
+  initialState: initialLoginState,
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.pending, (login) => {
+      login.loading = true;
+      login.error = null;
+    });
+
+    builder.addCase(loginUser.fulfilled, (login, action) => {
+      login.loading = false;
+      login.success = true;
+      login.isLoggedIn = true;
+      login.user = action.payload.data;
+      login.token = action.payload.data.token;
+      auth.saveUser(login.user);
+      auth.saveAuthToken(login.user.token);
+    });
+
+    builder.addCase(loginUser.rejected, (login, action) => {
+      login.loading = false;
+      login.success = false;
+      login.error = action.payload;
+    });
+  },
 });
 
-export { registerUser };
+export default combineReducers({
+  userRegister: registerSlice.reducer,
+  userLogin: loginSlice.reducer,
+});
+
+export { registerUser, loginUser };
