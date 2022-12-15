fe19cdfb8fc3e3fb8e1240580d2586bc02a31bfa
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-15 15:22:54 +0530

    Build register screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './screens/Home';
+import Register from './screens/Register';
 import Logout from './common/Logout';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/logout' element={<Logout />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/' element={<Home />} />
       </Routes>
       <Footer />
