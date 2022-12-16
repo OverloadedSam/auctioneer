9d344111d94649426c9e10f667607a1bbc064f0a
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-16 16:21:50 +0530

    Add login screen to routing of client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './screens/Home';
 import Register from './screens/Register';
+import Login from './screens/Login';
 import Logout from './common/Logout';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/logout' element={<Logout />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<Home />} />
       </Routes>
