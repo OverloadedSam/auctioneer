e858d1a25b71861d7bf676ae6d5bda70ae9fb9a0
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-17 15:49:33 +0530

    Build create auction screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RequireAuth from './common/RequireAuth';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './screens/Home';
 import Register from './screens/Register';
 import Login from './screens/Login';
+import CreateAuction from './screens/CreateAuction';
 import Logout from './common/Logout';
 
 const App = () => {
@@ -12,6 +14,14 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
+        <Route
+          path='/auction/create'
+          element={
+            <RequireAuth>
+              <CreateAuction />
+            </RequireAuth>
+          }
+        />
         <Route path='/logout' element={<Logout />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
