8502cd9a663d98a3b1f9a794d90bdfe90cc8b438
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-29 15:12:40 +0530

    Validation for incoming user profile update

--- server/validations/user.js
+++ server/validations/user.js
@@ -90,6 +90,54 @@ const validateLogin = async (payload) => {
   }
 };
 
+const validateUserData = async (payload) => {
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .min(3)
+      .max(32)
+      .trim()
+      .matches(/^[a-zA-Z ]{3,32}$/, {
+        message:
+          'Please provide valid name. Avoid numbers and special characters',
+      })
+      .label('Name'),
+    email: yup.string().email().label('E-mail'),
+    mobile: yup
+      .string()
+      .trim()
+      .matches(/^[6789]\d{9}$/, {
+        message: 'Please provide a valid Indian mobile number!',
+      })
+      .label('Mobile No.'),
+    dateOfBirth: yup
+      .date()
+      .max(get18YearsOldDate(), 'You must be at least 18 years old.')
+      .typeError('Please provide a valid date!')
+      .label('Date of Birth'),
+    password: yup.string().min(6).label('Password'),
+  });
+
+  try {
+    const data = await schema.validate(payload);
+
+    return {
+      isValid: true,
+      data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      isValid: false,
+      error: {
+        key: error.path,
+        value: error.params.originalValue,
+        message: error.message,
+      },
+    };
+  }
+};
+
 const isValidUserAvatar = (avatar) => {
   const allowedTypes = ['jpg', 'jpeg', 'png', 'webp'];
   const allowedSizeInBytes = 2 * 1024 * 1024; // 2MB is the file (image) size limit.
@@ -106,5 +154,6 @@ const isValidUserAvatar = (avatar) => {
 module.exports = {
   validateRegister,
   validateLogin,
+  validateUserData,
   isValidUserAvatar,
 };
