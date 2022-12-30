e7b88775b0564b15ea145ef2edbe2832f0c8cdc7
Samdeesh <samdeeshsingh001@gmail.com>
2022-12-30 12:42:54 +0530

    Add functionality to show contact details of winner or seller

--- client/src/components/AuctionWinner.jsx
+++ client/src/components/AuctionWinner.jsx
@@ -1,17 +1,42 @@
-import { useSelector } from 'react-redux';
-import Avatar from '@mui/material/Avatar';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
 import NoPerson from '@mui/icons-material/VoiceOverOff';
-import { getHighestBid, selectAuctionData } from '../features/auctionSlice';
+import Alert from '../common/Alert';
+import {
+  getHighestBid,
+  getWinnerSellerDetails,
+  resetWinnerSellerDetails,
+  selectWinnerSellerDetails,
+  selectAuctionData,
+} from '../features/auctionSlice';
 import { formatCurrency } from '../utils/utilityFunctions';
 
 const AuctionWinner = () => {
-  const { winner } = useSelector(selectAuctionData);
+  const dispatch = useDispatch();
+  const { _id, seller, winner } = useSelector(selectAuctionData);
+  const { loading, error, data } = useSelector(selectWinnerSellerDetails);
   const highestBid = useSelector(getHighestBid);
   const { user } = useSelector((state) => state.auth.userLogin);
+
+  const requestContactDetails = () => {
+    dispatch(getWinnerSellerDetails({ auctionId: _id }));
+  };
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetWinnerSellerDetails());
+    };
+  }, []);
 
   return (
     <Card sx={{ p: 2 }}>
@@ -65,6 +90,63 @@ const AuctionWinner = () => {
           </Typography>
         </CardContent>
       </CardActionArea>
+      <CardActions sx={{ flexDirection: 'column' }}>
+        {winner && winner._id === user.id ? (
+          <Button
+            onClick={requestContactDetails}
+            color='error'
+            variant='contained'
+            sx={{ mx: 'auto' }}
+            disabled={loading || !!error || !!data}
+          >
+            {loading ? 'Please wait...' : 'Show Seller Contact Detail'}
+          </Button>
+        ) : winner && seller._id === user.id ? (
+          <Button
+            onClick={requestContactDetails}
+            color='error'
+            variant='contained'
+            sx={{ mx: 'auto' }}
+            disabled={loading || !!error || !!data}
+          >
+            {loading ? 'Please wait...' : 'Show Winner Contact Detail'}
+          </Button>
+        ) : null}
+
+        <Stack mt={winner ? 3 : 0}>
+          {loading ? (
+            <CircularProgress color='secondary' />
+          ) : error ? (
+            <Alert message={error} variant='outlined' />
+          ) : data ? (
+            <>
+              <Typography gutterBottom variant='subtitle1' textAlign='center'>
+                Name: {data.contactDetails.name}
+              </Typography>
+              <Typography gutterBottom variant='subtitle1' textAlign='center'>
+                E-mail:{' '}
+                <Link
+                  href={`mailto:${data.contactDetails.email}`}
+                  rel='noopener noreferrer'
+                  underline='hover'
+                >
+                  {data.contactDetails.email}
+                </Link>
+              </Typography>
+              <Typography gutterBottom variant='subtitle1' textAlign='center'>
+                Mobile:{' '}
+                <Link
+                  href={`tel:+91-${data.contactDetails.mobile}`}
+                  rel='noopener noreferrer'
+                  underline='hover'
+                >
+                  +91-{data.contactDetails.mobile}
+                </Link>
+              </Typography>
+            </>
+          ) : null}
+        </Stack>
+      </CardActions>
     </Card>
   );
 };
